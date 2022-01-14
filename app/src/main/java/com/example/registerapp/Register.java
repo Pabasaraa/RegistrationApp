150a6d604b835260e57fab1dccc590979e779c84
Pabasaraa <pabasara.was@gmail.com>
2022-01-14 09:38:06 +0530

    Added Notification when registering.

--- app/src/main/java/com/example/registerapp/Register.java
+++ app/src/main/java/com/example/registerapp/Register.java
@@ -1,8 +1,14 @@
 package com.example.registerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +23,19 @@ public class Register extends AppCompatActivity {
     RadioButton male, female;
     Button reg;
     String gender;
+    public String CHANNEL_ID = "Register Confirm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Registered", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("This confirms that the user registration process has completed successfully.");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
         name = findViewById(R.id.etNameR);
         username = findViewById(R.id.etUserNameR);
@@ -46,7 +60,19 @@ public class Register extends AppCompatActivity {
                 Toast.makeText(Register.this, "Registered Successfully! ID:"+rowID, Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(getApplicationContext(), Profile.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(Register.this, 1, intent, 0);
                 startActivity(intent);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Register.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("You have registered successfully")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(Register.this);
+                notificationManagerCompat.notify(0, builder.build());
             }
         });
     }
